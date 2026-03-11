2057c7ac548335051de9a12403cee62f2146b0e1
Zaid Khan <khanzaid75005@gmail.com>
2026-03-11 20:28:30 +0530

    update message

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
@@ -14,10 +15,10 @@ export default defineConfig(({ mode }) => ({
 
   plugins: [
     react(),
-    mode === "development" && componentTagger(),
+    mode === "development" && componentTagger()
   ].filter(Boolean),
 
-  base: "/",
+  base: process.env.VITE_BASE_PATH || "/Portfolio",
 
   resolve: {
     alias: {
