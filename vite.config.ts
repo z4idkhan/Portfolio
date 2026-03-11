5e7230b2ab83873e3084b471ce7375cb0a3a2df5
Zaid Khan <khanzaid75005@gmail.com>
2026-03-11 20:39:50 +0530

    fix vercel routing

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
@@ -15,10 +14,10 @@ export default defineConfig(({ mode }) => ({
 
   plugins: [
     react(),
-    mode === "development" && componentTagger()
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
 
-  base: process.env.VITE_BASE_PATH || "/Portfolio",
+  base: "/",
 
   resolve: {
     alias: {
